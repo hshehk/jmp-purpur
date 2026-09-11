e30fc11cb2913235debdd097c0d6d0663599f52d
hshehk <yoyo1004198@gmail.com>
2026-09-11 20:07:52 +0800

    Add files via upload

--- src/main/java/top/archiem/jmp/util/TitleHandler.java
+++ src/main/java/top/archiem/jmp/util/TitleHandler.java
@@ -3,13 +3,9 @@ package top.archiem.jmp.util;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.title.Title;
-import net.kyori.adventure.title.TitlePart;
 import net.kyori.adventure.title.Title.Times;
-import org.bukkit.block.data.type.TechnicalPiston;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.annotations.UnknownNullability;
 import top.archiem.jmp.JMP;
 
 public class TitleHandler {
@@ -21,31 +17,13 @@ public class TitleHandler {
     }
 
     public Title titleBuilder(String title, String subtitle, @Nullable Times times){
-        Title titleinbuild = new Title() {
-            @Override
-            public @NotNull Component title() {
-                return tfInstance.format(title, player);
-            }
+        Component titleComponent = tfInstance.format(title, player);
+        Component subtitleComponent = tfInstance.format(subtitle, player);
 
-            @Override
-            public @NotNull Component subtitle() {
-                return tfInstance.format(subtitle, player);
-            }
-
-            @Override
-            public @Nullable Times times() {
-                if(times != null){
-                    return times;
-                }
-                return null;
-            }
-
-            @Override
-            public <T> @UnknownNullability T part(@NotNull TitlePart<T> part) {
-                return null;
-            }
-        };
-        return titleinbuild;
+        if (times != null) {
+            return Title.title(titleComponent, subtitleComponent, times);
+        }
+        return Title.title(titleComponent, subtitleComponent);
     }
 
 }
